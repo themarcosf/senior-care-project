0147144e2ab6c07f927f8cdb146a834a3f493816
Luiz Felipe <68920578+luiz-k-alencar@users.noreply.github.com>
2023-05-09 14:25:52 -0300

    login page created

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,7 +1,44 @@
+import styles from "@/styles/login.module.scss";
+import { FormEvent } from "react";
+
 export default function Login() {
+  const submitHandler = (event: FormEvent) => {
+    event.preventDefault();
+
+    console.log("Login");
+  };
+
   return (
-    <>
-      <h1>Hello World!</h1>
-    </>
-  )
+    <main className={styles.content}>
+      <div className={styles.imgBx}>
+        <img src="/logo.svg" alt="logo" />
+      </div>
+      <h1>
+        Welcome to <br /> Health <span>Care</span>
+      </h1>
+      <form className={styles.form} onSubmit={submitHandler}>
+        <div className={styles.field}>
+          <label htmlFor="email">E-mail</label>
+          <div>
+            <img src="/icons/email.svg" />
+            <input placeholder="Digite seu e-mail" id="email" type="email" />
+          </div>
+        </div>
+        <div className={styles.field}>
+          <label htmlFor="password">Senha</label>
+          <div>
+            <img src="/icons/padlock.svg" />
+            <input
+              placeholder="Insira sua senha"
+              id="password"
+              type="password"
+            />
+          </div>
+        </div>
+        <div className={styles.actions}>
+          <button type="submit">Entrar</button>
+        </div>
+      </form>
+    </main>
+  );
 }

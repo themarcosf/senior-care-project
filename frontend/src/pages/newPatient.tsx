d29d77d68ee930b16471565e238901f7ab0bb219
Luiz Felipe <68920578+luiz-k-alencar@users.noreply.github.com>
2023-05-31 20:44:30 -0300

    react loader

--- frontend/src/pages/newPatient.tsx
+++ frontend/src/pages/newPatient.tsx
@@ -1,18 +1,21 @@
+import { GetServerSideProps } from "next";
+import { useRouter } from "next/router";
 import { FormEvent, useState, useEffect } from "react";
 import InputMask from "react-input-mask";
 
+import api from "@/services/api";
+import useInput from "@/hooks/useInput";
+
 import Header from "@/components/Header/Header";
 
-import { useRouter } from "next/router";
+import { TailSpin } from "react-loader-spinner";
 
 import styles from "@/styles/newPatientPage.module.scss";
-import api from "@/services/api";
-import { GetServerSideProps } from "next";
-import useInput from "@/hooks/useInput";
 
 const NewPatientPage = () => {
   const route = useRouter();
   const [formIsValid, setFormIsValid] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
   const {
     value: enteredPatientName,
@@ -199,7 +202,10 @@ const NewPatientPage = () => {
   const saveHandler = async (event: FormEvent) => {
     event.preventDefault();
 
-    inputsSubmitHandler();
+    setIsLoading(true);
+
+    console.log("saveHandler");
+    // inputsSubmitHandler();
 
     if (!formIsValid) {
       return;
@@ -226,9 +232,11 @@ const NewPatientPage = () => {
 
     await api.post(`/med-record`, patientData);
 
-    resetInputsHandler();
+    setIsLoading(false);
 
     route.push("/patients"); //Após a   finalização do post, direcionar para uma nova evolução
+
+    resetInputsHandler();
   };
 
   return (
@@ -247,197 +255,218 @@ const NewPatientPage = () => {
 
       <main className={styles.main}>
         <div className={styles.formsHeader}>
-          <h1 onClick={() => console.log(formIsValid)}>Cadastro do paciente</h1>
+          <h1>Cadastro do paciente</h1>
           <button
             style={{ backgroundColor: formIsValid ? "#13a060" : "" }}
             form="registerForm"
+            disabled={!formIsValid}
           >
             Cadastrar
           </button>
         </div>
 
-        <form className={styles.form} id="registerForm" onSubmit={saveHandler}>
-          <div className={styles.smallFields}>
-            <div className={styles.fieldBlocks}>
-              <div className={styles.field}>
-                <label htmlFor="name">Nome Completo</label>
-                <input
-                  type="text"
-                  name="name"
-                  id="name"
-                  placeholder="Digite aqui..."
-                  value={enteredPatientName}
-                  onChange={patientNameChangeHandler}
-                  onBlur={patientNameBlurHandler}
-                />
-                {patientNameInputHasError && (
-                  <p className="error-text">Nome não deve estar vazio.</p>
-                )}
-              </div>
-              <div className={styles.field}>
-                <label htmlFor="patientPatientNationalId">CPF</label>
-                <InputMask
-                  id="patientPatientNationalId"
-                  mask="999.999.999-99"
-                  placeholder="Digite aqui..."
-                  value={enteredPatientNationalId}
-                  onChange={patientNationalIdChangeHandler}
-                  onBlur={patientNationalIdBlurHandler}
-                />
-                {patientNationalIdInputHasError && (
-                  <p className="error-text">CPF não deve estar vazio.</p>
-                )}
-              </div>
-              <div className={styles.field}>
-                <label htmlFor="ICD">CID</label>
-                <input
-                  type="text"
-                  name="ICD"
-                  id="ICD"
-                  placeholder="Digite aqui..."
-                  value={enteredIcdCode}
-                  onChange={icdCodeChangeHandler}
-                  onBlur={icdCodeBlurHandler}
-                />
-                {icdCodeInputHasError && (
-                  <p className="error-text">CID não deve estar vazio.</p>
-                )}
-              </div>
-              <div className={styles.field}>
-                <label htmlFor="birthDate">Data de nascimento</label>
-                <input
-                  type="date"
-                  name="birthDate"
-                  id="birthDate"
-                  value={enteredBirthDate}
-                  onChange={birthDateChangeHandler}
-                  onBlur={birthDateBlurHandler}
-                />
-                {birthDateInputHasError && (
-                  <p className="error-text">
-                    Data de nascimento não deve estar vazio.
-                  </p>
-                )}
-              </div>
-            </div>
-
-            <div className={styles.fieldBlocks}>
-              <div className={styles.field}>
-                <label htmlFor="legalGuardian">Responsável Legal</label>
-                <input
-                  type="text"
-                  name="legalGuardian"
-                  id="legalGuardian"
-                  placeholder="Digite aqui..."
-                  value={enteredLegalGuardianName}
-                  onChange={legalGuardianNameChangeHandler}
-                  onBlur={legalGuardianNameBlurHandler}
-                />
-                {legalGuardianNameInputHasError && (
-                  <p className="error-text">
-                    Responsável não deve estar vazio.
-                  </p>
-                )}
-              </div>
-              <div className={styles.field}>
-                <label htmlFor="legalGuardianNationalId">
-                  CPF do Responsável
-                </label>
-                <InputMask
-                  id="legalGuardianNationalId"
-                  mask="999.999.999-99"
-                  placeholder="Digite aqui..."
-                  value={enteredLegalGuardianNationalId}
-                  onChange={legalGuardianNationalIdChangeHandler}
-                  onBlur={legalGuardianNationalIdBlurHandler}
-                />
-                {legalGuardianNationalIdInputHasError && (
-                  <p className="error-text">
-                    CPF do responsável legal não deve estar vazio.
-                  </p>
-                )}
-              </div>
-              <div className={styles.field}>
-                <label htmlFor="legalGuardianPhone">Tel para contato</label>
-                <InputMask
-                  type="tel"
-                  id="legalGuardianPhone"
-                  mask="(99) 99999-9999"
-                  placeholder="Digite aqui..."
-                  value={enteredLegalGuardianPhone}
-                  onChange={legalGuardianPhoneChangeHandler}
-                  onBlur={legalGuardianPhoneBlurHandler}
-                />
-                {legalGuardianPhoneInputHasError && (
-                  <p className="error-text">Telefone não deve estar vazio.</p>
-                )}
-              </div>
-              <div className={styles.field}>
-                <label htmlFor="legalGuardianEmail">Email para contato</label>
-                <input
-                  type="email"
-                  name="legalGuardianEmail"
-                  id="legalGuardianEmail"
-                  placeholder="Digite aqui..."
-                  value={enteredLegalGuardianEmail}
-                  onChange={legalGuardianEmailChangeHandler}
-                  onBlur={legalGuardianEmailBlurHandler}
-                />
-                {legalGuardianEmailInputHasError && (
-                  <p className="error-text">Email não deve estar vazio.</p>
-                )}
-              </div>
-            </div>
-
-            <div className={styles.fieldBlocks}>
-              <div className={styles.field}>
-                <label htmlFor="insurancePlan">Plano de saúde</label>
-                <input
-                  type="text"
-                  name="insurancePlan"
-                  id="insurancePlan"
-                  placeholder="Digite aqui..."
-                  value={enteredInsurancePlan}
-                  onChange={insurancePlanChangeHandler}
-                  onBlur={insurancePlanBlurHandler}
-                />
-                {insurancePlanInputHasError && (
-                  <p className="error-text">
-                    Plano de Saúde não deve estar vazio.
-                  </p>
-                )}
-              </div>
-              <div className={styles.field}>
-                <label htmlFor="insurancePolicyNumber">Nº do Plano</label>
-                <input
-                  type="text"
-                  name="insurancePolicyNumber"
-                  id="insurancePolicyNumber"
-                  placeholder="Digite aqui..."
-                  value={enteredInsurancePolicyNumber}
-                  onChange={insurancePolicyNumberChangeHandler}
-                  onBlur={insurancePolicyNumberBlurHandler}
-                />
-                {insurancePolicyNumberInputHasError && (
-                  <p className="error-text">
-                    Nº do Plano não deve estar vazio.
-                  </p>
-                )}
-              </div>
-            </div>
+        {isLoading && (
+          <div className={styles.spinnerContainer}>
+            <TailSpin
+              height="80"
+              width="80"
+              color="var(--neutral-400)"
+              ariaLabel="tail-spin-loading"
+              radius="1"
+              wrapperStyle={{}}
+              wrapperClass=""
+              visible={true}
+            />
           </div>
-          <div className={`${styles.field} ${styles.descriptionfield}`}>
-            <label htmlFor="observation">Observações</label>
-            <textarea
-              name="observation"
-              id="observation"
-              placeholder="Digite aqui..."
-              value={enteredObservation}
-              onChange={observationChangeHandler}
-              onBlur={observationBlurHandler}
-            ></textarea>
-          </div>
-        </form>
+        )}
+        {!isLoading && (
+          <form
+            className={styles.form}
+            id="registerForm"
+            onSubmit={saveHandler}
+          >
+            <div className={styles.smallFields}>
+              <div className={styles.fieldBlocks}>
+                <div className={styles.field}>
+                  <label htmlFor="name">Nome Completo</label>
+                  <input
+                    type="text"
+                    name="name"
+                    id="name"
+                    placeholder="Digite aqui..."
+                    value={enteredPatientName}
+                    onChange={patientNameChangeHandler}
+                    onBlur={patientNameBlurHandler}
+                  />
+                  {patientNameInputHasError && (
+                    <p className="error-text">Nome não deve estar vazio.</p>
+                  )}
+                </div>
+                <div className={styles.field}>
+                  <label htmlFor="patientPatientNationalId">CPF</label>
+                  <InputMask
+                    id="patientPatientNationalId"
+                    mask="999.999.999-99"
+                    placeholder="Digite aqui..."
+                    value={enteredPatientNationalId}
+                    onChange={patientNationalIdChangeHandler}
+                    onBlur={patientNationalIdBlurHandler}
+                  />
+                  {patientNationalIdInputHasError && (
+                    <p className="error-text">CPF não deve estar vazio.</p>
+                  )}
+                </div>
+                <div className={styles.field}>
+                  <label htmlFor="ICD">CID</label>
+                  <input
+                    type="text"
+                    name="ICD"
+                    id="ICD"
+                    placeholder="Digite aqui..."
+                    value={enteredIcdCode}
+                    onChange={icdCodeChangeHandler}
+                    onBlur={icdCodeBlurHandler}
+                  />
+                  {icdCodeInputHasError && (
+                    <p className="error-text">CID não deve estar vazio.</p>
+                  )}
+                </div>
+                <div className={styles.field}>
+                  <label htmlFor="birthDate">Data de nascimento</label>
+                  <input
+                    type="date"
+                    name="birthDate"
+                    id="birthDate"
+                    value={enteredBirthDate}
+                    onChange={birthDateChangeHandler}
+                    onBlur={birthDateBlurHandler}
+                  />
+                  {birthDateInputHasError && (
+                    <p className="error-text">
+                      Data de nascimento não deve estar vazio.
+                    </p>
+                  )}
+                </div>
+              </div>
+
+              <div className={styles.fieldBlocks}>
+                <div className={styles.field}>
+                  <label htmlFor="legalGuardian">Responsável Legal</label>
+                  <input
+                    type="text"
+                    name="legalGuardian"
+                    id="legalGuardian"
+                    placeholder="Digite aqui..."
+                    value={enteredLegalGuardianName}
+                    onChange={legalGuardianNameChangeHandler}
+                    onBlur={legalGuardianNameBlurHandler}
+                  />
+                  {legalGuardianNameInputHasError && (
+                    <p className="error-text">
+                      Responsável não deve estar vazio.
+                    </p>
+                  )}
+                </div>
+                <div className={styles.field}>
+                  <label htmlFor="legalGuardianNationalId">
+                    CPF do Responsável
+                  </label>
+                  <InputMask
+                    id="legalGuardianNationalId"
+                    mask="999.999.999-99"
+                    placeholder="Digite aqui..."
+                    value={enteredLegalGuardianNationalId}
+                    onChange={legalGuardianNationalIdChangeHandler}
+                    onBlur={legalGuardianNationalIdBlurHandler}
+                  />
+                  {legalGuardianNationalIdInputHasError && (
+                    <p className="error-text">
+                      CPF do responsável legal não deve estar vazio.
+                    </p>
+                  )}
+                </div>
+                <div className={styles.field}>
+                  <label htmlFor="legalGuardianPhone">Tel para contato</label>
+                  <InputMask
+                    type="tel"
+                    id="legalGuardianPhone"
+                    mask="(99) 99999-9999"
+                    placeholder="Digite aqui..."
+                    value={enteredLegalGuardianPhone}
+                    onChange={legalGuardianPhoneChangeHandler}
+                    onBlur={legalGuardianPhoneBlurHandler}
+                  />
+                  {legalGuardianPhoneInputHasError && (
+                    <p className="error-text">Telefone não deve estar vazio.</p>
+                  )}
+                </div>
+                <div className={styles.field}>
+                  <label htmlFor="legalGuardianEmail">Email para contato</label>
+                  <input
+                    type="email"
+                    name="legalGuardianEmail"
+                    id="legalGuardianEmail"
+                    placeholder="Digite aqui..."
+                    value={enteredLegalGuardianEmail}
+                    onChange={legalGuardianEmailChangeHandler}
+                    onBlur={legalGuardianEmailBlurHandler}
+                  />
+                  {legalGuardianEmailInputHasError && (
+                    <p className="error-text">Email não deve estar vazio.</p>
+                  )}
+                </div>
+              </div>
+
+              <div className={styles.fieldBlocks}>
+                <div className={styles.field}>
+                  <label htmlFor="insurancePlan">Plano de saúde</label>
+                  <input
+                    type="text"
+                    name="insurancePlan"
+                    id="insurancePlan"
+                    placeholder="Digite aqui..."
+                    value={enteredInsurancePlan}
+                    onChange={insurancePlanChangeHandler}
+                    onBlur={insurancePlanBlurHandler}
+                  />
+                  {insurancePlanInputHasError && (
+                    <p className="error-text">
+                      Plano de Saúde não deve estar vazio.
+                    </p>
+                  )}
+                </div>
+                <div className={styles.field}>
+                  <label htmlFor="insurancePolicyNumber">Nº do Plano</label>
+                  <input
+                    type="text"
+                    name="insurancePolicyNumber"
+                    id="insurancePolicyNumber"
+                    placeholder="Digite aqui..."
+                    value={enteredInsurancePolicyNumber}
+                    onChange={insurancePolicyNumberChangeHandler}
+                    onBlur={insurancePolicyNumberBlurHandler}
+                  />
+                  {insurancePolicyNumberInputHasError && (
+                    <p className="error-text">
+                      Nº do Plano não deve estar vazio.
+                    </p>
+                  )}
+                </div>
+              </div>
+            </div>
+            <div className={`${styles.field} ${styles.descriptionfield}`}>
+              <label htmlFor="observation">Observações</label>
+              <textarea
+                name="observation"
+                id="observation"
+                placeholder="Digite aqui..."
+                value={enteredObservation}
+                onChange={observationChangeHandler}
+                onBlur={observationBlurHandler}
+              ></textarea>
+            </div>
+          </form>
+        )}
       </main>
     </>
   );

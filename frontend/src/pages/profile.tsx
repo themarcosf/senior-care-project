11e97e7947b62200862effea3d5dcdfbf4396c2c
Luiz Felipe <68920578+luiz-k-alencar@users.noreply.github.com>
2023-06-01 10:12:47 -0300

    profile created

--- frontend/src/pages/profile.tsx
+++ frontend/src/pages/profile.tsx
@@ -1,21 +1,99 @@
-import styles from '@/styles/profile.module.scss'
+import { useRouter } from "next/router";
+import Cookies from "js-cookie";
+import { BiLogOut } from "react-icons/bi";
 
-import Cookies from 'js-cookie';
-import { useRouter } from 'next/router';
+import styles from "@/styles/profile.module.scss";
+import { GetServerSideProps } from "next";
+import { FC } from "react";
+import { profile } from "@/models/profile";
 
-const profile = () => {
-    const route = useRouter();
+const Profile: FC<{
+  profileData: profile;
+  statistics: {
+    medRecordTotal: number;
+    medProgressionTotal: number;
+  };
+}> = ({ profileData, statistics }) => {
+  const route = useRouter();
 
-    const signoutHandler = () => {
-        Cookies.remove('token');
-        route.push('/');
+  const signoutHandler = () => {
+    // Cookies.remove("token");
+    // route.push("/");
+  };
+
+  return (
+    <div className={styles.content}>
+      <div className={styles.header}>
+        <h1>Perfil</h1>
+        <button onClick={signoutHandler}>
+          <BiLogOut size={25} fill="var(--neutral-400)" />
+          Sair
+        </button>
+      </div>
+      <div className={styles.profile}>
+        <div className={styles.profileImage}>
+          <img
+            // src={profileData.avatar}
+            src="https://pbs.twimg.com/media/FjU2lkcWYAgNG6d.jpg"
+            alt="Avatar"
+          />
+        </div>
+        <div className={styles.profileInfo}>
+          <h2>{profileData.name}</h2>
+          <p>{profileData.role}</p>
+          <p>{profileData.licenseNum}</p>
+        </div>
+      </div>
+      <div className={styles.statistics}>
+        <div>
+          <h3>{statistics.medRecordTotal}</h3>
+          <p>Prontuários</p>
+        </div>
+        <div>
+          <h3>{statistics.medProgressionTotal}</h3>
+          <p>Evoluções</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { req } = context;
+
+  const token = req.cookies["token"];
+
+  if (!token) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
+  const profileResponse = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/auth/profile`,
+    {
+      headers: {
+        Authorization: "Bearer " + token,
+      },
     }
+  );
 
-     return (
-       <div className={styles.content}>
-          <button onClick={signoutHandler}>Sair</button>
-       </div>
-   )
-}
+  const profileData = await profileResponse.json();
 
-export default profile;
+  const statistics = {
+    medRecordTotal: 0,
+    medProgressionTotal: 0,
+  };
+
+  return {
+    props: {
+      profileData,
+      statistics,
+    },
+  };
+};
